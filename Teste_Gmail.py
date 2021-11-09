2b1649bfcbabbb98104d3d07cf5bbe431b771359
João Pedro Müller Wilges <80231608+Joao-Pedro-MW@users.noreply.github.com>
2021-11-08 23:28:32 -0300

    Update Teste_Gmail.py

--- Teste_Gmail.py
+++ Teste_Gmail.py
@@ -1,27 +1,9 @@
 import smtplib
 
 server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("jpedro01230@gmail.com", "ggcurgnzefufudqo")
+server.login("E-mail@gmail.com", "Senha_de_login_de_apps")
 server.sendmail(
-  "jpedro01230@gmail.com",
-  "wilges.joao@escola.pr.gov.br",
-  "teste")
+  "E-mail@gmail.com",
+  "E-mail_de_envio@gmail.com",
+  "Texto da mensage")
 server.quit()
-
-
-"""from pyautogui import *
-from time import sleep
-import pyautogui as pgui
-# clica no chrome
-pgui.click(x=737, y=751)
-sleep(2)
-#clica no perfil
-pgui.click(x=595, y=410)
-sleep(2)
-pgui.hotkey('ctrl', 't') 
-sleep(1)
-pgui.write("gmail.com")
-sleep(1)
-pgui.press("enter")
-sleep(6)
-pgui.press("c")"""
