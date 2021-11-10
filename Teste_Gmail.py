d34908d8c7a7708e6d937146b014251b4768223e
João Pedro Müller Wilges <80231608+Joao-Pedro-MW@users.noreply.github.com>
2021-11-09 22:30:26 -0300

    Update Teste_Gmail.py

--- Teste_Gmail.py
+++ Teste_Gmail.py
@@ -1,9 +1,1 @@
-import smtplib
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login("E-mail@gmail.com", "Senha_de_login_de_apps")
-server.sendmail(
-  "E-mail@gmail.com",
-  "E-mail_de_envio@gmail.com",
-  "Texto da mensage")
-server.quit()
