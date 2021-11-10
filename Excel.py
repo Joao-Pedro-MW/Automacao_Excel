1095f48469ceb9086b4e450141c20e75d022b1d4
João Pedro Müller Wilges <80231608+Joao-Pedro-MW@users.noreply.github.com>
2021-11-09 22:29:44 -0300

    Upload das versões para GMAIL

--- Excel.py
+++ Excel.py
@@ -1,38 +1,26 @@
+from SMTP import enviar_email
+from tkinter import messagebox
 from openpyxl import *
-from pyautogui import *
-from time import sleep
-import pyautogui as pgui
-#abrir o arquivo
-nomes = list()
-emails = list()
-caminho = "C:/Users/USUARIO/OneDrive/Área de Trabalho/Pycel/Data.xlsx"
-planilha = load_workbook(caminho)
+
+#abre o arquivo
+nomes,emails = list(),list()
+messagebox.showwarning("AVISO!", "PROCESSANDO DADOS, AGUARDE...")
+planilha = load_workbook("Data.xlsx")
 planilha = planilha["Nomes"]
-for row in planilha.iter_rows(min_row=1, max_row=300, min_col=1, max_col=1,values_only=True):
 #este loop acessa os valores na primeira coluna
+for row in planilha.iter_rows(min_row=2, min_col=1, max_col=1,values_only=True):
     for cell in row: 
-        # com o parametro FOR CELL IN ROW, convertemos para String os dados
-        #sem o parametro os dados são retornados em tuple
         nomes.append(cell)
-for col in planilha.iter_cols(min_col= 2, max_col=2, max_row=300, values_only=True):
-    #este loop acessa os valores na segunda coluna
+#este loop acessa os valores na segunda coluna
+for col in planilha.iter_cols(min_row=2, min_col=2, max_col=2,values_only=True):
     for cell in col:
         emails.append(cell)
-
-#print(emails)
-#print(nomes)
-#abre o gmail pra enviar o arquivo
-pgui.hotkey('win','r')
-pgui.write("chrome")
-pgui.press("enter")
-sleep(2)
-#clica no perfil
-pgui.click(x=595, y=410)
-sleep(2)
-pgui.hotkey('ctrl', 't') 
-sleep(1)
-pgui.write("gmail.com")
-sleep(1)
-pgui.press("enter")
-sleep(6)
-pgui.press("c")
+#Gmail 
+email_remetente = input("Informe o seu e-mail: ")
+senha_remetente = input("Informe a sua senha de login: ")
+assunto = str(input("Qual o assunto da mensagem a ser passada à todos os e-mails? "))
+for n in range(len(nomes)):
+    nome_do_destinatario = nomes[n]
+    email_usuario = emails[n]
+    print("Email sendo enviado para {user} em nome de {name}".format(user = email_usuario, name = nome_do_destinatario))
+    enviar_email(email_remetente,email_usuario,senha_remetente,assunto,nome_do_destinatario)
---
+++ SMTP.py
@@ -0,0 +1,48 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from tkinter import messagebox
+
+def enviar_email(remetente,destinatario,senha_login,assunto,nome_remetente):
+  message = MIMEMultipart("alternative")
+  message["Subject"] = assunto
+  message["From"] = remetente
+  message["To"] = destinatario
+  # Crie o texto padrão em o texto em HTML
+  text = """\
+  Olá {nome}, tudo bem?
+  Seu cadastro está em análise em noso banco de dados :D"""
+  html = """\
+  <html>
+    <body>
+      <p>Olá {nome}, tudo bem?<br>
+        Seu cadastro está em análise em noso banco de dados :D</p>
+    </body>
+  </html>
+  """.format(nome = nome_remetente)
+  # Converta para texto normal (plain)  e html
+  part1 = MIMEText(text, "plain")
+  part2 = MIMEText(html, "html")
+  # Anexe as mensagem do e-mail, a ultima parte é renderizada primeiro
+  message.attach(part1)
+  message.attach(part2)
+  # Cria uma conexão segura com o servidor e envia o e-mail
+  context = ssl.create_default_context()
+  with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+      try:
+        server.login(remetente, senha_login)
+        try:
+          server.sendmail(remetente, destinatario, message.as_string())
+        except TypeError:
+          messagebox.showerror("ERRO NA CÉLULA DA PLANILHA","Verifique se a planilha está preenchida corretamente")
+          pass
+      except smtplib.SMTPAuthenticationError:
+        messagebox.showerror("Erro de login", "Login e/ou senha inválidos")
+        pass
+        
+      
+
+  
+
+
+
